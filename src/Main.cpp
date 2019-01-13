b02de8cab9c95308cbbcb4429027c21fc67c69d7
William Ciesialka <wciesialka@gmail.com>
2019-01-12 23:06:51 -0500

    Added empty Grid to Main and rendered it

--- src/Main.cpp
+++ src/Main.cpp
@@ -3,20 +3,33 @@
 #include "Grid.h"
 #include "Cell.h"
 
+Grid g;
+
 void render(sf::RenderWindow* window)
 {
     window->clear();
 
-    float r = 100.0;
-    sf::CircleShape circle(r);
-    circle.setFillColor(sf::Color::White);
-    circle.setPosition(WIDTH/2 - r,HEIGHT/2 - r);
-
-    window->draw(circle);
+    for(int j = 0; j < GRID_HEIGHT; j++)
+    {
+        for(int i = 0; i < GRID_WIDTH; i++)
+        {
+            sf::RectangleShape space(sf::Vector2f(GRID_SIZE,GRID_SIZE));
+            space.setPosition(i*GRID_SIZE,j*GRID_SIZE);
+            if(g.getCell(i,j)->isWhite())
+            {
+                space.setFillColor(sf::Color::White);
+            } else
+            {
+                space.setFillColor(sf::Color::Black);
+            }
+            window->draw(space);
+        }
+    }
 }
 
 int main(int argc, char** argv)
 {
+    g = Grid();
     sf::RenderWindow window(sf::VideoMode(WIDTH, HEIGHT),"Langton's Ant");
 
     while(window.isOpen())
