8992d79dbc9cee52e8acf30e65f24225a4e1dab9
William Ciesialka <wciesialka@gmail.com>
2019-01-12 22:38:31 -0500

    Added Cell class

---
+++ src/Cell.h
@@ -0,0 +1,14 @@
+#ifndef _CELL_H_
+#define _CELL_H_
+
+class Cell 
+{
+    private:
+        bool white;
+    public:
+        Cell(bool white);
+        bool isWhite();
+        void invert();
+};
+
+#endif
