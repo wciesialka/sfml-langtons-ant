6e8d894f6d46ffd197eed02b31382437c3d8c4b0
William Ciesialka <wciesialka@gmail.com>
2019-01-12 23:42:08 -0500

    Got Ant to render and move successfully

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,51 +1,69 @@
 #include <SFML/Graphics.hpp>
-#include <iostream>
+#include <thread>
+#include <chrono>
+//#include <iostream>
+
 #include "Grid.h"
 #include "Cell.h"
+#include "Ant.h"
 
-Grid g;
+#define MILLIS_DELAY 5
 
-void render(sf::RenderWindow* window)
+Grid *g;
+Ant *ant;
+sf::RenderWindow *window;
+
+void update()
 {
-    window->clear();
-
-    for(int j = 0; j < GRID_HEIGHT; j++)
+    int x = ant->getX();
+    int y = ant->getY();
+    sf::RectangleShape space(sf::Vector2f(GRID_SIZE,GRID_SIZE));
+    space.setPosition(x*GRID_SIZE,y*GRID_SIZE);
+    if(g->getCell(x,y)->isWhite())
     {
-        for(int i = 0; i < GRID_WIDTH; i++)
-        {
-            sf::RectangleShape space(sf::Vector2f(GRID_SIZE,GRID_SIZE));
-            space.setPosition(i*GRID_SIZE,j*GRID_SIZE);
-            if(g.getCell(i,j)->isWhite())
-            {
-                space.setFillColor(sf::Color::White);
-            } else
-            {
-                space.setFillColor(sf::Color::Black);
-            }
-            window->draw(space);
-        }
+        ant->turnLeft();
+        space.setFillColor(sf::Color::Black);
+    } else
+    {
+        ant->turnRight();
+        space.setFillColor(sf::Color::White);
     }
+    g->getCell(x,y)->invert();
+    window->draw(space);
+    ant->moveForward();
 }
 
 int main(int argc, char** argv)
 {
-    g = Grid();
-    sf::RenderWindow window(sf::VideoMode(WIDTH, HEIGHT),"Langton's Ant");
+    g = new Grid();
+    ant = new Ant(GRID_WIDTH/2,GRID_HEIGHT/2);
+    window = new sf::RenderWindow(sf::VideoMode(WIDTH, HEIGHT),"Langton's Ant");
+    sf::RectangleShape space(sf::Vector2f(WIDTH,HEIGHT));
+    space.setFillColor(sf::Color::White);
+    window->draw(space);
+    window->display();
 
-    while(window.isOpen())
+    while(window->isOpen())
     {
+        //std::cout << "Main loop!" << std::endl;
         sf::Event ev;
-        while(window.pollEvent(ev))
+        while(window->pollEvent(ev))
         {
             if(ev.type == sf::Event::Closed)
             {
-                window.close();
+                window->close();
             }
         }
 
-        render(&window);
-        window.display();
+        update();
+        window->display();
+
+        std::this_thread::sleep_for (std::chrono::milliseconds(MILLIS_DELAY));
     }
+
+    delete g;
+    delete window;
+    delete ant;
 
     return 0;
 }
